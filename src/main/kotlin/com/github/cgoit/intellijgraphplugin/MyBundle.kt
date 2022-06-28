3ce4fe38d493e156eeb7ff38752598829db0bddd
GitHub Action <action@github.com>
2022-06-28 04:45:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/cgoit/intellijgraphplugin/MyBundle.kt
+++ src/main/kotlin/com/github/cgoit/intellijgraphplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cgoit.intellijgraphplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cgoit/intellijgraphplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cgoit/intellijgraphplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cgoit.intellijgraphplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cgoit.intellijgraphplugin.MyBundle
 
 class MyApplicationService {
 
